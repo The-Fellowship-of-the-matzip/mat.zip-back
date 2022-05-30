c6bef546ed9a2631428c8213234a58f37ebc1493
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-31 00:32:41 +0900

    refactor: 패키지 이동

--- src/test/java/com/woowacourse/MatzipApplicationTests.java
+++ src/test/java/com/woowacourse/MatzipApplicationTests.java
@@ -1,7 +1,6 @@
-package com.woowacourse.matzip;
+package com.woowacourse;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class MatzipApplicationTests {
 
