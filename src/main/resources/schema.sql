705434189e9801ccd49fc2eb2a01b6276626ad57
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-31 00:09:30 +0900

    feat: init project setup

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,46 @@
+CREATE TABLE member
+(
+    id            bigint       NOT NULL,
+    github_id     varchar(255) NOT NULL UNIQUE,
+    username      varchar(255) NOT NULL,
+    profile_image varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE restaurant
+(
+    id            bigint       NOT NULL,
+    category_id   bigint       NOT NULL,
+    campus_id     bigint       NOT NULL,
+    name          varchar(20)  NOT NULL,
+    address       varchar(255) NOT NULL UNIQUE,
+    distance      bigint       NOT NULL,
+    kakao_map_url varchar(255) NOT NULL,
+    image_url     varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE review
+(
+    id            bigint       NOT NULL,
+    member_id     bigint       NOT NULL,
+    restaurant_id bigint       NOT NULL,
+    content       varchar(255) NULL,
+    score         int          NOT NULL,
+    menu          varchar(20)  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE category
+(
+    id   bigint      NOT NULL,
+    name varchar(10) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE campus
+(
+    id   bigint      NOT NULL,
+    name varchar(20) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
---
+++ src/test/java/com/woowacourse/matzip/MatzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.matzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class MatzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+        MatzipApplication.main(new String[]{});
+    }
+
+}
