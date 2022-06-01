5879fe3d568392c45dcadcf092f6feddcf5f2487
jayjaehunchoi <wogns0108@naver.com>
2022-06-01 14:04:04 +0900

    feat: Member 도메인 생성

---
+++ src/main/java/com/woowacourse/matzip/domain/member/Member.java
@@ -0,0 +1,59 @@
+package com.woowacourse.matzip.domain.member;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(name = "UN_GITHUB_ID", columnNames = "github_id"))
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "github_id", nullable = false, unique = true)
+    private String githubId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "profile_image", nullable = false)
+    private String profileImage;
+
+    protected Member() {
+    }
+
+    @Builder
+    public Member(final Long id, final String githubId, final String name, final String profileImage) {
+        this.id = id;
+        this.githubId = githubId;
+        this.name = name;
+        this.profileImage = profileImage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
