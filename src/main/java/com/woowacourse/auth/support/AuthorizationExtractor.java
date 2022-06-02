6b630c6b5359293b29c3c8325d23bcabcc7c9408
jayjaehunchoi <wogns0108@naver.com>
2022-06-02 16:33:46 +0900

    refactor: AuthorizationExtractor 리팩터링

---
+++ src/main/java/com/woowacourse/auth/support/AuthorizationExtractor.java
@@ -0,0 +1,31 @@
+package com.woowacourse.auth.support;
+
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.util.Strings;
+
+public class AuthorizationExtractor {
+
+    private static final String AUTHORIZATION_HEADER_TYPE = "Authrozation";
+    private static final String TOKEN_TYPE = "Bearer";
+
+    private AuthorizationExtractor() {
+        throw new AssertionError();
+    }
+
+    public static Optional<String> extract(final HttpServletRequest request) {
+        String header = request.getHeader(AUTHORIZATION_HEADER_TYPE);
+        if (Strings.isEmpty(header)) {
+            return Optional.empty();
+        }
+        return checkMatch(header.split(" "));
+    }
+
+    private static Optional<String> checkMatch(final String[] parts) {
+        if (parts.length == 2 && parts[0].equals(TOKEN_TYPE)) {
+            return Optional.ofNullable(parts[1]);
+        }
+        return Optional.empty();
+    }
+}
+
