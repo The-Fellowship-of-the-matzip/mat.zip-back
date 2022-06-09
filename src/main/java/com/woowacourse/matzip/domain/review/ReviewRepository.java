26ba7fc5f132b7087b8b480c4bc929188d40796a
Ohzzi <clover7even@naver.com>
2022-06-10 01:28:54 +0900

    feat: Review entity 구현

---
+++ src/main/java/com/woowacourse/matzip/domain/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package com.woowacourse.matzip.domain.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
