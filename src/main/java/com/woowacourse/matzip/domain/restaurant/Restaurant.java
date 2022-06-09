6caa2702116348162096a9ca4869c009a242d7e9
Ohzzi <clover7even@naver.com>
2022-06-09 17:57:28 +0900

    feat: Restaurant entity 구현

---
+++ src/main/java/com/woowacourse/matzip/domain/restaurant/Restaurant.java
@@ -0,0 +1,75 @@
+package com.woowacourse.matzip.domain.restaurant;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Table(name = "restaurant")
+@Getter
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_id", nullable = false)
+    private Long categoryId;
+
+    @Column(name = "campus_id", nullable = false)
+    private Long campusId;
+
+    @Column(name = "name", length = 20, nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false, unique = true)
+    private String address;
+
+    @Column(name = "distance", nullable = false)
+    private Long distance;
+
+    @Column(name = "kakao_map_url", nullable = false)
+    private String kakaoMapUrl;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    protected Restaurant() {
+    }
+
+    @Builder
+    public Restaurant(final Long id, final Long categoryId, final Long campusId, final String name,
+                      final String address, final Long distance, final String kakaoMapUrl, final String imageUrl) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.campusId = campusId;
+        this.name = name;
+        this.address = address;
+        this.distance = distance;
+        this.kakaoMapUrl = kakaoMapUrl;
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Restaurant that = (Restaurant) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/woowacourse/matzip/domain/restaurant/RestaurantRepository.java
@@ -0,0 +1,6 @@
+package com.woowacourse.matzip.domain.restaurant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+}
