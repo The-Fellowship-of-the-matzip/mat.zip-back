36245f14956532a1ce168b414a0bd4c6cb747c32
jayjaehunchoi <wogns0108@naver.com>
2022-06-09 21:52:25 +0900

    feat: AuthConfig에 인터셉터, 아규먼트리졸버 등록

---
+++ src/main/java/com/woowacourse/auth/presentation/AuthenticationArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.woowacourse.auth.presentation;
+
+import com.woowacourse.auth.support.AuthenticationPrincipal;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class AuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final AuthenticationContext authenticationContext;
+
+    public AuthenticationArgumentResolver(final AuthenticationContext authenticationContext) {
+        this.authenticationContext = authenticationContext;
+    }
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
+    }
+
+    @Override
+    public String resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory)
+            throws Exception {
+        return authenticationContext.getAuthority();
+    }
+}
