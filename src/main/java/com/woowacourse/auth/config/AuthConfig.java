f5aa9495e9516f1fef2680de857d2a7a01f3ad08
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-06-11 18:23:31 +0900

    feat: config path 수정

--- src/main/java/com/woowacourse/auth/config/AuthConfig.java
+++ src/main/java/com/woowacourse/auth/config/AuthConfig.java
@@ -23,7 +23,7 @@ public class AuthConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/api/campus/*/reviews");
+                .addPathPatterns("/api/campuses/*/reviews");
     }
 
     @Override
