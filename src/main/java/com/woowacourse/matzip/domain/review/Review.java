e98687a100f0b50c0c65d365b6a4c64e5348483c
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-06-13 18:29:23 +0900

    feat: Review member값 객체로 지정

--- src/main/java/com/woowacourse/matzip/domain/review/Review.java
+++ src/main/java/com/woowacourse/matzip/domain/review/Review.java
@@ -1,11 +1,15 @@
 package com.woowacourse.matzip.domain.review;
 
+import com.woowacourse.matzip.domain.member.Member;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,8 +23,9 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "member_id", nullable = false)
-    private Long memberId;
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
 
     @Column(name = "restaurant_id", nullable = false)
     private Long restaurantId;
@@ -38,10 +43,10 @@ public class Review {
     }
 
     @Builder
-    public Review(final Long id, final Long memberId, final Long restaurantId, final String content,
+    public Review(final Long id, final Member member, final Long restaurantId, final String content,
                   final Integer score, final String menu) {
         this.id = id;
-        this.memberId = memberId;
+        this.member = member;
         this.restaurantId = restaurantId;
         this.content = content;
         this.score = score;
