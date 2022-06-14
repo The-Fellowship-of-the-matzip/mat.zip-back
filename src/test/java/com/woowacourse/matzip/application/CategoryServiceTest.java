f692df6101f66b6d7bbb9230c9bd5a4709add322
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-06-14 09:42:54 +0900

    feat: data.sql 복구

--- src/test/java/com/woowacourse/matzip/application/CategoryServiceTest.java
+++ src/test/java/com/woowacourse/matzip/application/CategoryServiceTest.java
@@ -1,10 +1,5 @@
 package com.woowacourse.matzip.application;
 
-import static com.woowacourse.matzip.support.CategoryFixtures.양식;
-import static com.woowacourse.matzip.support.CategoryFixtures.일식;
-import static com.woowacourse.matzip.support.CategoryFixtures.중식;
-import static com.woowacourse.matzip.support.CategoryFixtures.카페_디저트;
-import static com.woowacourse.matzip.support.CategoryFixtures.한식;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.woowacourse.matzip.application.response.CategoryResponse;
@@ -25,12 +20,6 @@ class CategoryServiceTest {
 
     @Test
     void 카테고리_전체_목록을_반환한다() {
-        categoryRepository.save(한식.toCategory());
-        categoryRepository.save(중식.toCategory());
-        categoryRepository.save(일식.toCategory());
-        categoryRepository.save(양식.toCategory());
-        categoryRepository.save(카페_디저트.toCategory());
-
         List<CategoryResponse> responses = categoryService.findAll();
         assertThat(responses).hasSize(5);
     }
--- src/test/java/com/woowacourse/matzip/application/ReviewServiceTest.java
+++ src/test/java/com/woowacourse/matzip/application/ReviewServiceTest.java
@@ -1,21 +1,14 @@
 package com.woowacourse.matzip.application;
 
 import static com.woowacourse.auth.support.GithubResponseFixtures.HUNI;
-import static com.woowacourse.matzip.support.CategoryFixtures.한식;
-import static com.woowacourse.matzip.support.RestaurantFixture.RESTAURANT_1;
 import static com.woowacourse.matzip.support.ReviewFixtures.REVIEW_1;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import com.woowacourse.matzip.domain.campus.Campus;
-import com.woowacourse.matzip.domain.campus.CampusRepository;
-import com.woowacourse.matzip.domain.category.Category;
-import com.woowacourse.matzip.domain.category.CategoryRepository;
 import com.woowacourse.matzip.domain.member.Member;
 import com.woowacourse.matzip.domain.member.MemberRepository;
 import com.woowacourse.matzip.domain.restaurant.Restaurant;
 import com.woowacourse.matzip.domain.restaurant.RestaurantRepository;
-import com.woowacourse.matzip.domain.review.ReviewRepository;
 import com.woowacourse.matzip.exception.MemberNotFoundException;
 import com.woowacourse.matzip.presentation.request.ReviewCreateRequest;
 import com.woowacourse.support.SpringServiceTest;
@@ -29,16 +22,7 @@ public class ReviewServiceTest {
     private ReviewService reviewService;
 
     @Autowired
-    private ReviewRepository reviewRepository;
-
-    @Autowired
     private RestaurantRepository restaurantRepository;
-
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    private CampusRepository campusRepository;
 
     @Autowired
     private MemberRepository memberRepository;
@@ -56,12 +40,8 @@ public class ReviewServiceTest {
     @Test
     void 리뷰를_작성한다() {
         Member member = memberRepository.save(HUNI.toMember());
-        Campus campus = campusRepository.findAll().get(0);
-        Category category = categoryRepository.save(한식.toCategory());
-        Restaurant restaurant = restaurantRepository
-                .save(RESTAURANT_1.toRestaurantWithCategoryIdAndCampusId(category.getId(), campus.getId()));
+        Restaurant restaurant = restaurantRepository.findAll().get(0);
 
-        assertDoesNotThrow(
-                () -> reviewService.createReview(member.getGithubId(), restaurant.getId(), reviewCreateRequest()));
+        assertDoesNotThrow(() -> reviewService.createReview(member.getGithubId(), restaurant.getId(), reviewCreateRequest()));
     }
 }
