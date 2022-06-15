6277429dfb8afde31530f9b4c2899562d54b0bfd
Ohzzi <clover7even@naver.com>
2022-06-15 19:26:00 +0900

    test: static 메서드 위치 조정

--- src/test/java/com/woowacourse/acceptance/RestaurantAcceptanceTest.java
+++ src/test/java/com/woowacourse/acceptance/RestaurantAcceptanceTest.java
@@ -13,6 +13,10 @@ class RestaurantAcceptanceTest extends AcceptanceTest {
     private static final Long 선릉캠퍼스_ID = 2L;
     private static final Long 한식_ID = 1L;
 
+    private static ExtractableResponse<Response> 캠퍼스_식당_0페이지_조회_요청(final Long campusId) {
+        return httpGetRequest("/api/campuses/" + campusId + "/restaurants?page=0&size=10");
+    }
+
     @Test
     void 선릉캠퍼스_식당_목록의_0페이지를_조회한다() {
         ExtractableResponse<Response> response = 캠퍼스_식당_0페이지_조회_요청(선릉캠퍼스_ID);
@@ -23,10 +27,6 @@ class RestaurantAcceptanceTest extends AcceptanceTest {
     void 선릉캠퍼스_한식_식당_목록의_0페이지를_조회한다() {
         ExtractableResponse<Response> response = 캠퍼스_카테고리_식당_0페이지_조회_요청(선릉캠퍼스_ID, 한식_ID);
         식당_조회에_성공한다(response);
-    }
-
-    private static ExtractableResponse<Response> 캠퍼스_식당_0페이지_조회_요청(final Long campusId) {
-        return httpGetRequest("/api/campuses/" + campusId + "/restaurants?page=0&size=10");
     }
 
     private ExtractableResponse<Response> 캠퍼스_카테고리_식당_0페이지_조회_요청(Long campusId, Long categoryId) {
