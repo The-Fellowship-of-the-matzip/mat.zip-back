e57f9162bc717885c247f7ed50ab35fb9a27c1c3
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-06-20 13:41:26 +0900

    feat: 토큰예외에 대한 401 예외 핸들링 추가

--- src/main/java/com/woowacourse/auth/presentation/AuthControllerAdvice.java
+++ src/main/java/com/woowacourse/auth/presentation/AuthControllerAdvice.java
@@ -1,7 +1,10 @@
 package com.woowacourse.auth.presentation;
 
 import com.woowacourse.auth.exception.GithubAccessException;
+import com.woowacourse.auth.exception.InvalidTokenException;
+import com.woowacourse.auth.exception.TokenNotFoundException;
 import com.woowacourse.support.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +15,13 @@ public class AuthControllerAdvice {
     @ExceptionHandler(GithubAccessException.class)
     public ResponseEntity<ErrorResponse> githubAccessExceptionHandler(final GithubAccessException e) {
         return ResponseEntity.badRequest().body(ErrorResponse.from(e));
+    }
+
+    @ExceptionHandler({
+            InvalidTokenException.class,
+            TokenNotFoundException.class
+    })
+    public ResponseEntity<ErrorResponse> tokenExceptionHandler(final RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.from(e));
     }
 }
