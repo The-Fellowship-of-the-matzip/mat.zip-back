0902bd39df33e7ca43d5d702dc49c5873587bac4
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-06-24 18:38:58 +0900

    feat: 길이 validation 메시지 수정

--- src/main/java/com/woowacourse/matzip/support/LengthValidator.java
+++ src/main/java/com/woowacourse/matzip/support/LengthValidator.java
@@ -10,13 +10,13 @@ public class LengthValidator {
 
     public static void checkStringLength(final String value, final int max, final String name) {
         if (value.length() > max) {
-            throw new InvalidLengthException(String.format("%s는 %d보다 작은 값만 입력할 수 있습니다.", name, max));
+            throw new InvalidLengthException(String.format("%s은(는) %d보다 작은 값만 입력할 수 있습니다.", name, max));
         }
     }
 
     public static void checkStringLength(final String value, final int max, final int min, final String name) {
         if (value.length() < min || value.length() > max) {
-            throw new InvalidLengthException(String.format("%s는 %d와 %d 사이 값만 입력할 수 있습니다.", name, min, max));
+            throw new InvalidLengthException(String.format("%s은(는) %d와 %d 사이 값만 입력할 수 있습니다.", name, min, max));
         }
     }
 }
