3c687e528561509945da58ce3407c410f0b66dba
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-06-24 18:39:10 +0900

    feat: 캠퍼스 이름 길이 제한 기능 추가 및 테스트

--- src/test/java/com/woowacourse/matzip/domain/campus/CampusTest.java
+++ src/test/java/com/woowacourse/matzip/domain/campus/CampusTest.java
@@ -1,17 +1,14 @@
 package com.woowacourse.matzip.domain.campus;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.woowacourse.matzip.exception.InvalidLengthException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class CampusTest {
-
-    @Test
-    void 캠퍼스_생성() {
-        assertThat(new Campus(1L, "JAMSIL")).isInstanceOf(Campus.class);
-    }
 
     @ParameterizedTest
     @CsvSource(value = {"1,true", "2,false"})
@@ -19,5 +16,14 @@ public class CampusTest {
         Campus campus = new Campus(1L, "JAMSIL");
 
         assertThat(campus.isSameId(id)).isEqualTo(expected);
+    }
+
+    @Test
+    void 캠퍼스_생성_시_이름_길이_제한() {
+        String name = "캠퍼스의 이름이 이렇게 길 수 없습니다.";
+
+        assertThatThrownBy(() -> Campus.builder().name(name).build())
+                .isInstanceOf(InvalidLengthException.class)
+                .hasMessage("캠퍼스 이름은(는) 20보다 작은 값만 입력할 수 있습니다.");
     }
 }
