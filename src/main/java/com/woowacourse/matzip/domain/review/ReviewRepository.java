9d8d140f13cb19d8ba4b6b27f8fe534ada20ce55
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-06-25 18:03:31 +0900

    feat: review paging 조회 시 member fetch join 추가

--- src/main/java/com/woowacourse/matzip/domain/review/ReviewRepository.java
+++ src/main/java/com/woowacourse/matzip/domain/review/ReviewRepository.java
@@ -3,6 +3,8 @@ package com.woowacourse.matzip.domain.review;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,5 +16,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     )
     Optional<Double> findAverageRatingUsingRestaurantId(@Param("restaurantId") Long restaurantId);
 
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraphType.FETCH)
     Slice<Review> findPageByRestaurantIdOrderByIdDesc(Long restaurantId, Pageable pageable);
 }
