8ca2039a91c930075b0939cbafa025bac4f2c511
Ohzzi <clover7even@naver.com>
2022-07-10 00:21:55 +0900

    refactor: 더 이상 사용하지 않는 메서드 삭제

--- src/main/java/com/woowacourse/matzip/domain/restaurant/RestaurantRepository.java
+++ src/main/java/com/woowacourse/matzip/domain/restaurant/RestaurantRepository.java
@@ -5,19 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-
-    @Query(
-            value = "select r from Restaurant r "
-                    + "where "
-                    + "(r.campusId = :campusId) "
-                    + "and "
-                    + "(:categoryId is null or r.categoryId = :categoryId) "
-    )
-    Slice<Restaurant> findPageByCampusId(@Param("campusId") Long campusId, @Param("categoryId") Long categoryId,
-                                         Pageable pageable);
 
     @Query(
             value = "select id, category_id, campus_id, name, address, distance, kakao_map_url, image_url "
