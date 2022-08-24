5ef25f0cbe9cacdc7288e1ab0c8a947a0efaee2c
jayjaehunchoi <wogns0108@naver.com>
2022-08-24 17:46:48 +0900

    feat: 인터셉터 프록시 패턴 적용

---
+++ src/main/java/com/woowacourse/auth/presentation/interceptor/PathMatcherInterceptor.java
@@ -0,0 +1,36 @@
+package com.woowacourse.auth.presentation.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class PathMatcherInterceptor implements HandlerInterceptor {
+
+    private final HandlerInterceptor handlerInterceptor;
+    private final PathContainer pathContainer;
+
+    public PathMatcherInterceptor(final HandlerInterceptor handlerInterceptor) {
+        this.handlerInterceptor = handlerInterceptor;
+        this.pathContainer = new PathContainer();
+    }
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request,
+                             final HttpServletResponse response,
+                             final Object handler) throws Exception {
+        if (pathContainer.notIncludedPath(request.getServletPath(), request.getMethod())) {
+            return true;
+        }
+        return handlerInterceptor.preHandle(request, response, handler);
+    }
+
+    public PathMatcherInterceptor includePathPattern(final String pathPattern, final PathMethod pathMethod) {
+        pathContainer.includePathPattern(pathPattern, pathMethod);
+        return this;
+    }
+
+    public PathMatcherInterceptor excludePathPattern(final String pathPattern, final PathMethod pathMethod) {
+        pathContainer.excludePathPattern(pathPattern, pathMethod);
+        return this;
+    }
+}
