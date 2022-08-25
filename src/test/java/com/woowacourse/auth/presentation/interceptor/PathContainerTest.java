6681c02a3b330836eeca88a8d952982e343a21d6
jayjaehunchoi <wogns0108@naver.com>
2022-08-25 16:07:37 +0900

    test: false인 경우 테스트 코드 추가

--- src/test/java/com/woowacourse/auth/presentation/interceptor/PathContainerTest.java
+++ src/test/java/com/woowacourse/auth/presentation/interceptor/PathContainerTest.java
@@ -3,6 +3,8 @@ package com.woowacourse.auth.presentation.interceptor;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PathContainerTest {
 
@@ -11,6 +13,14 @@ class PathContainerTest {
         PathContainer pathContainer = new PathContainer();
         pathContainer.includePathPattern("/api/reviews", PathMethod.DELETE);
         assertThat(pathContainer.isNotIncludedPath("/api/reviews", "PUT")).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"GET", "POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS"})
+    void include에_포함된_경로(String method) {
+        PathContainer pathContainer = new PathContainer();
+        pathContainer.includePathPattern("/api/reviews", PathMethod.ANY);
+        assertThat(pathContainer.isNotIncludedPath("/api/reviews", method)).isFalse();
     }
 
     @Test
