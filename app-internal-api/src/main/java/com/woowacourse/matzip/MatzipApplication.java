f776f55d8b3d0a384985bdfbec65fa6c19ac9a7c
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-26 03:29:49 +0900

    chore: add admin module

---
+++ app-internal-api/src/main/java/com/woowacourse/matzip/MatzipApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.matzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatzipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatzipApplication.class, args);
+    }
+
+}
