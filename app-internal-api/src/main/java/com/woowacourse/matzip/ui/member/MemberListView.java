fc1bafc5c0e90f50b92e6fb186893ebe19ed8def
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-26 20:34:26 +0900

    feat: 유저 프로필 목록 가시화 수정

--- app-internal-api/src/main/java/com/woowacourse/matzip/ui/member/MemberListView.java
+++ app-internal-api/src/main/java/com/woowacourse/matzip/ui/member/MemberListView.java
@@ -2,6 +2,9 @@ package com.woowacourse.matzip.ui.member;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.LitRenderer;
+import com.vaadin.flow.data.renderer.LocalDateTimeRenderer;
+import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import com.woowacourse.matzip.application.AdminMemberService;
 import com.woowacourse.matzip.domain.member.Member;
@@ -22,16 +25,32 @@ public class MemberListView extends VerticalLayout {
     }
 
     private Grid<Member> createMemberGrid() {
-        Grid<Member> grid = new Grid<>();
+        Grid<Member> grid = new Grid<>(Member.class, false);
         grid.setSizeFull();
         grid.addColumn(Member::getId).setHeader("id");
         grid.addColumn(Member::getGithubId).setHeader("github id");
-        grid.addColumn(Member::getUsername).setHeader("username");
+        grid.addColumn(createMemberProfileRenderer()).setHeader("username");
         grid.addColumn(Member::getProfileImage).setHeader("profile image url");
-        grid.addColumn(Member::getCreatedAt).setHeader("created date");
+        grid.addColumn(new LocalDateTimeRenderer<>(Member::getCreatedAt, "yyyy-MM-dd hh:mm:ss"))
+                .setHeader("created date")
+                .setComparator(Member::getCreatedAt);
+
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+        grid.getColumns().forEach(col -> col.setSortable(true));
 
         grid.setItems(adminMemberService.findAll());
         return grid;
+    }
+
+    private static Renderer<Member> createMemberProfileRenderer() {
+        return LitRenderer.<Member>of(
+                "<vaadin-horizontal-layout style=\"align-items: center;\" theme=\"spacing\">"
+                        + "  <vaadin-avatar img=\"${item.profileImage}\" name=\"${item.username}\"></vaadin-avatar>"
+                        + "  <vaadin-vertical-layout style=\"line-height: var(--lumo-line-height-m);\">"
+                        + "    <span> ${item.username} </span>"
+                        + "  </vaadin-vertical-layout>"
+                        + "</vaadin-horizontal-layout>")
+                .withProperty("profileImage", Member::getProfileImage)
+                .withProperty("username", Member::getUsername);
     }
 }
