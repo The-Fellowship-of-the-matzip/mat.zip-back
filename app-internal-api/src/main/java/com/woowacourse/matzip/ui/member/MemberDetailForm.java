0169f5a1fcedeb4f203c5dc2b53e6cfa2e0d1e07
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-27 04:12:56 +0900

    refactor: memberdetailform field 설정방법 변경

--- app-internal-api/src/main/java/com/woowacourse/matzip/ui/member/MemberDetailForm.java
+++ app-internal-api/src/main/java/com/woowacourse/matzip/ui/member/MemberDetailForm.java
@@ -3,26 +3,29 @@ package com.woowacourse.matzip.ui.member;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 
 public class MemberDetailForm extends FormLayout {
+
+    private final TextField id = new TextField("id");
+    private final TextField githubId = new TextField("github d");
+    private final TextField username = new TextField("username");
+    private final TextField profileImage = new TextField("profile image url");
+    private final TextField createdAt = new TextField("created date");
 
     public MemberDetailForm() {
         addClassName("contact-form");
 
         add(
                 new H2("멤버 상세 정보"),
-                new TextField("id"),
-                new TextField("github id"),
-                new TextField("username"),
-                new TextField("profile image url"),
-                new TextField("created date"),
+                id,
+                githubId,
+                username,
+                profileImage,
+                createdAt,
                 createButtonsLayout()
         );
     }
@@ -38,7 +41,6 @@ public class MemberDetailForm extends FormLayout {
         Button close = new Button("취소");
         close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
         close.addClickShortcut(Key.ESCAPE);
-
 
         return new HorizontalLayout(save, delete, close);
     }
