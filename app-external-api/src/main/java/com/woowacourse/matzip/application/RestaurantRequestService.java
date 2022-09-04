c9b9c9c19e4a0f45c766fe440f6a16cec74992d4
Ohzzi <clover7even@naver.com>
2022-09-04 17:26:45 +0900

    feat: 식당 추가 요청 조회 service 구현

--- app-external-api/src/main/java/com/woowacourse/matzip/application/RestaurantRequestService.java
+++ app-external-api/src/main/java/com/woowacourse/matzip/application/RestaurantRequestService.java
@@ -1,11 +1,14 @@
 package com.woowacourse.matzip.application;
 
+import com.woowacourse.matzip.application.response.RestaurantRequestsResponse;
 import com.woowacourse.matzip.domain.member.Member;
 import com.woowacourse.matzip.domain.member.MemberRepository;
 import com.woowacourse.matzip.domain.restaurant.RestaurantRequest;
 import com.woowacourse.matzip.domain.restaurant.RestaurantRequestRepository;
 import com.woowacourse.matzip.exception.MemberNotFoundException;
 import com.woowacourse.matzip.presentation.request.RestaurantRequestCreateRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +28,13 @@ public class RestaurantRequestService {
     @Transactional
     public void createRequest(final String githubId, final Long campusId,
                               final RestaurantRequestCreateRequest request) {
-        Member member = memberRepository.findMemberByGithubId(githubId)
-                .orElseThrow(MemberNotFoundException::new);
+        Member member = memberRepository.findMemberByGithubId(githubId).orElseThrow(MemberNotFoundException::new);
         RestaurantRequest restaurantRequest = request.toRestaurantRequestWithMemberAndCampusId(member, campusId);
         restaurantRequestRepository.save(restaurantRequest);
+    }
+
+    public RestaurantRequestsResponse findPage(final String githubId, final Long campusId, final Pageable pageable) {
+        Slice<RestaurantRequest> page = restaurantRequestRepository.findPageByCampusId(campusId, pageable);
+        return RestaurantRequestsResponse.of(page, githubId);
     }
 }
--- app-external-api/src/test/java/com/woowacourse/matzip/application/RestaurantRequestServiceTest.java
+++ app-external-api/src/test/java/com/woowacourse/matzip/application/RestaurantRequestServiceTest.java
@@ -2,15 +2,19 @@ package com.woowacourse.matzip.application;
 
 import static com.woowacourse.auth.support.GithubResponseFixtures.ORI;
 import static com.woowacourse.matzip.support.RestaurantRequestFixtures.RESTAURANT_REQUEST_1;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import com.woowacourse.matzip.application.response.RestaurantRequestsResponse;
 import com.woowacourse.matzip.domain.member.Member;
 import com.woowacourse.matzip.domain.member.MemberRepository;
 import com.woowacourse.matzip.presentation.request.RestaurantRequestCreateRequest;
 import com.woowacourse.support.SpringServiceTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 
 @SpringServiceTest
 class RestaurantRequestServiceTest {
@@ -37,5 +41,20 @@ class RestaurantRequestServiceTest {
 
         assertDoesNotThrow(() -> restaurantRequestService.createRequest(member.getGithubId(), 1L,
                 restaurantRequestCreateRequest()));
+    }
+
+    @Test
+    void 캠퍼스_별_식당_추가_요청을_조회한다() {
+        식당_추가_요청을_생성한다();
+
+        RestaurantRequestsResponse response = restaurantRequestService.findPage(ORI.getGithubId(), 1L,
+                PageRequest.of(0, 1));
+
+        assertAll(
+                () -> assertThat(response.getItems()).hasSize(1)
+                        .extracting("updatable")
+                        .containsExactly(true), // 변경 가능한지(작성자인지) 확인
+                () -> assertThat(response.isHasNext()).isTrue()
+        );
     }
 }
