292142eab24a5e13f79659cec4780462d8916f6d
Ohzzi <clover7even@naver.com>
2022-09-13 17:49:22 +0900

    refactor: Slice 객체를 파싱하는 로직을 DTO로 이동

--- app-external-api/src/main/java/com/woowacourse/matzip/application/response/RestaurantRequestsResponse.java
+++ app-external-api/src/main/java/com/woowacourse/matzip/application/response/RestaurantRequestsResponse.java
@@ -1,7 +1,10 @@
 package com.woowacourse.matzip.application.response;
 
+import com.woowacourse.matzip.domain.restaurant.RestaurantRequest;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
+import org.springframework.data.domain.Slice;
 
 @Getter
 public class RestaurantRequestsResponse {
@@ -15,5 +18,13 @@ public class RestaurantRequestsResponse {
     public RestaurantRequestsResponse(final List<RestaurantRequestResponse> items, final boolean hasNext) {
         this.items = items;
         this.hasNext = hasNext;
+    }
+
+    public static RestaurantRequestsResponse of(final Slice<RestaurantRequest> page, final String githubId) {
+        List<RestaurantRequestResponse> items = page.getContent()
+                .stream()
+                .map(restaurantRequest -> RestaurantRequestResponse.of(restaurantRequest, githubId))
+                .collect(Collectors.toList());
+        return new RestaurantRequestsResponse(items, page.hasNext());
     }
 }
