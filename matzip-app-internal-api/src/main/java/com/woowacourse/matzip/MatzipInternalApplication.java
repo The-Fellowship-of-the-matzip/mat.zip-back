e76a7aa824639d252c5bfdc5a50f33cb8ccd5e72
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-16 20:09:40 +0900

    refactor: Application class 네이밍 구체화

--- matzip-app-internal-api/src/main/java/com/woowacourse/matzip/MatzipInternalApplication.java
+++ matzip-app-internal-api/src/main/java/com/woowacourse/matzip/MatzipInternalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatzipApplication {
+public class MatzipInternalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MatzipApplication.class, args);
+        SpringApplication.run(MatzipInternalApplication.class, args);
     }
 
 }
