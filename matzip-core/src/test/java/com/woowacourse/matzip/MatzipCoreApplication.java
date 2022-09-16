336842c7c9d703056f7b5e107f0c1d74362be941
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-09-16 20:46:59 +0900

    refactor: enternal-app -> core repository test 이동

---
+++ matzip-core/src/test/java/com/woowacourse/matzip/MatzipCoreApplication.java
@@ -0,0 +1,12 @@
+package com.woowacourse.matzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatzipCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatzipCoreApplication.class, args);
+    }
+}
