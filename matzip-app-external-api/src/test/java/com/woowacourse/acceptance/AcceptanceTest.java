ff909c9e811b3cb986b75150bb6d1d94759165df
kevstevie <jianna442@gmail.com>
2023-06-11 19:23:25 +0900

    test: 테스트 격리 방식 변경

--- matzip-app-external-api/src/test/java/com/woowacourse/acceptance/AcceptanceTest.java
+++ matzip-app-external-api/src/test/java/com/woowacourse/acceptance/AcceptanceTest.java
@@ -1,8 +1,11 @@
 package com.woowacourse.acceptance;
 
+import com.woowacourse.support.DataClearManager;
 import com.woowacourse.support.SpringAcceptanceTest;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringAcceptanceTest
@@ -11,8 +14,16 @@ public class AcceptanceTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private DataClearManager dataClearManager;
+
     @BeforeEach
     void setPort() {
         RestAssured.port = port;
+    }
+
+    @AfterEach
+    void afterEach() {
+        dataClearManager.clear();
     }
 }
