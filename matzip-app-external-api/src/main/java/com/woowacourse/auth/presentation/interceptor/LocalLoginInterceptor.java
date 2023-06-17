3343becbcc4486449fef726ca543b032ed03bb3f
Woojin Lee <dhkep03@gmail.com>
2023-06-17 17:48:27 +0900

    chore: Profile 별로 다른 Interceptor 사용

---
+++ matzip-app-external-api/src/main/java/com/woowacourse/auth/presentation/interceptor/LocalLoginInterceptor.java
@@ -0,0 +1,34 @@
+package com.woowacourse.auth.presentation.interceptor;
+
+import com.woowacourse.auth.application.JwtTokenProvider;
+import com.woowacourse.auth.exception.TokenNotFoundException;
+import com.woowacourse.auth.presentation.AuthenticationContext;
+import com.woowacourse.auth.support.AuthorizationExtractor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("local")
+@Component
+public class LocalLoginInterceptor implements LoginInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationContext authenticationContext;
+
+    public LocalLoginInterceptor(final JwtTokenProvider jwtTokenProvider,
+                                 final AuthenticationContext authenticationContext) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationContext = authenticationContext;
+    }
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request,
+                             final HttpServletResponse response,
+                             final Object handler) {
+        final String token = AuthorizationExtractor.extract(request)
+                .orElseThrow(TokenNotFoundException::new);
+        authenticationContext.setAuthenticationMember(token);
+        return true;
+    }
+}
--- matzip-app-external-api/src/main/java/com/woowacourse/auth/presentation/interceptor/LoginInterceptor.java
+++ matzip-app-external-api/src/main/java/com/woowacourse/auth/presentation/interceptor/LoginInterceptor.java
@@ -1,50 +1,6 @@
 package com.woowacourse.auth.presentation.interceptor;
 
-import com.woowacourse.auth.application.JwtTokenProvider;
-import com.woowacourse.auth.exception.TokenNotFoundException;
-import com.woowacourse.auth.presentation.AuthenticationContext;
-import com.woowacourse.auth.support.AuthorizationExtractor;
-import java.util.Arrays;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-@Component
-public class LoginInterceptor implements HandlerInterceptor {
-
-    private static final String PROFILE_LOCAL = "local";
-
-    @Autowired
-    private final Environment environment;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final AuthenticationContext authenticationContext;
-
-    public LoginInterceptor(Environment environment, final JwtTokenProvider jwtTokenProvider,
-                            final AuthenticationContext authenticationContext) {
-        this.environment = environment;
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.authenticationContext = authenticationContext;
-    }
-
-    @Override
-    public boolean preHandle(final HttpServletRequest request,
-                             final HttpServletResponse response,
-                             final Object handler) {
-        final String token = AuthorizationExtractor.extract(request)
-                .orElseThrow(TokenNotFoundException::new);
-        if (isLocalPhase()) {
-            authenticationContext.setAuthenticationMember(token);
-            return true;
-        }
-        authenticationContext.setAuthenticationMember(jwtTokenProvider.getPayload(token));
-        return true;
-    }
-
-    private boolean isLocalPhase() {
-        return Arrays.stream(environment.getActiveProfiles())
-                .anyMatch(profile -> profile.equalsIgnoreCase(PROFILE_LOCAL));
-    }
+public interface LoginInterceptor extends HandlerInterceptor {
 }
