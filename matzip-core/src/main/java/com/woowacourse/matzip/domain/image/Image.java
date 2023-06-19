b8e2c36c48690ac995427c2baab3719e34ca6af3
kevstevie <jianna442@gmail.com>
2023-06-19 16:46:30 +0900

    feat: image entity 추가

---
+++ matzip-core/src/main/java/com/woowacourse/matzip/domain/image/Image.java
@@ -0,0 +1,33 @@
+package com.woowacourse.matzip.domain.image;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "image")
+@Getter
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    protected Image() {
+    }
+
+    @Builder
+    public Image(final Long id, final String imageUrl) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+    }
+}
