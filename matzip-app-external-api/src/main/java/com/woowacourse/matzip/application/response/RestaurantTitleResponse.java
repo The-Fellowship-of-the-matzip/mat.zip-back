4e1762ea4e35794906a5387a0f8e0da412c3697a
Woojin <97939198+beer-2000@users.noreply.github.com>
2023-06-30 23:02:50 +0900

    refactor: API 속성명 변경 (#184)
    * refactor: API 속성명 변경
    
    * refactor: 생성자 파라미터 개수 줄이기

--- matzip-app-external-api/src/main/java/com/woowacourse/matzip/application/response/RestaurantTitleResponse.java
+++ matzip-app-external-api/src/main/java/com/woowacourse/matzip/application/response/RestaurantTitleResponse.java
@@ -13,32 +13,19 @@ public class RestaurantTitleResponse {
     private double rating;
     private String imageUrl;
     private int reviewCount;
-    private boolean saved;
+    private boolean liked;
 
     private RestaurantTitleResponse() {
     }
 
-    private RestaurantTitleResponse(Long id, String name, String address, long distance, double rating, String imageUrl,
-                                    int reviewCount, boolean saved) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.distance = distance;
+    public RestaurantTitleResponse(final Restaurant restaurant, final double rating, final boolean liked) {
+        this.id = restaurant.getId();
+        this.name = restaurant.getName();
+        this.address = restaurant.getAddress();
+        this.distance = restaurant.getDistance();
         this.rating = rating;
-        this.imageUrl = imageUrl;
-        this.reviewCount = reviewCount;
-        this.saved = saved;
-    }
-
-    public static RestaurantTitleResponse of(final Restaurant restaurant, final double rating, final boolean saved) {
-        return new RestaurantTitleResponse(restaurant.getId(),
-                restaurant.getName(),
-                restaurant.getAddress(),
-                restaurant.getDistance(),
-                rating,
-                restaurant.getImageUrl(),
-                restaurant.getReviewCount(),
-                saved
-        );
+        this.imageUrl = restaurant.getImageUrl();
+        this.reviewCount = restaurant.getReviewCount();
+        this.liked = liked;
     }
 }
