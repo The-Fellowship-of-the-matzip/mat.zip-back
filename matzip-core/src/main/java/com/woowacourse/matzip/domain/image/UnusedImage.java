ea9ba83da13091dbd4fed7a4018c53c707a59805
kevstevie <jianna442@gmail.com>
2023-08-13 01:47:24 +0900

    feat: 사용하지 않는 이미지 삭제 기능

---
+++ matzip-core/src/main/java/com/woowacourse/matzip/domain/image/UnusedImage.java
@@ -0,0 +1,43 @@
+package com.woowacourse.matzip.domain.image;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Entity
+@Table(name = "unused_image")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class UnusedImage {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "key", nullable = false)
+    private String key;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    protected UnusedImage() {
+    }
+
+    @Builder
+    public UnusedImage(final Long id, final String key, final LocalDateTime createdAt) {
+        this.id = id;
+        this.key = key;
+        this.createdAt = createdAt;
+    }
+}
