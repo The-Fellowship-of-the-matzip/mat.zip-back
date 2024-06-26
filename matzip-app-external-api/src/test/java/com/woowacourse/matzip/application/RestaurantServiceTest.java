52576efd8a158768822be5c02b318eb6cc63f6cc
Hyeon0208 <dmsgktn0208@gmail.com>
2024-06-26 13:52:15 +0900

    refactor: 거리, 찜수 정렬 테스트 명에 후순위 정렬 조건 설명 추가

--- matzip-app-external-api/src/test/java/com/woowacourse/matzip/application/RestaurantServiceTest.java
+++ matzip-app-external-api/src/test/java/com/woowacourse/matzip/application/RestaurantServiceTest.java
@@ -126,7 +126,7 @@ class RestaurantServiceTest {
     }
 
     @Test
-    void 캠퍼스id와_카테고리id가_일치하는_식당을_가까운_거리순으로_페이징해서_반환한다() {
+    void 캠퍼스id와_카테고리id가_일치하는_식당을_가까운_거리순_가나다순으로_페이징해서_반환한다() {
         Restaurant restaurant1 = createTestRestaurant(1L, 1L, "식당1", "주소1", 5);
         Restaurant restaurant2 = createTestRestaurant(1L, 1L, "식당2", "주소2", 7);
         Restaurant restaurant3 = createTestRestaurant(1L, 1L, "식당3", "주소3", 6);
@@ -141,7 +141,7 @@ class RestaurantServiceTest {
     }
 
     @Test
-    void 캠퍼스id와_카테고리id가_일치하는_식당을_찜수가_많은순으로_페이징해서_반환한다() {
+    void 캠퍼스id와_카테고리id가_일치하는_식당을_찜수가_많은순_가나다순으로_페이징해서_반환한다() {
         Restaurant restaurant1 = createTestRestaurant(1L, 1L, "식당1", "주소1");
         Restaurant restaurant2 = createTestRestaurant(1L, 1L, "식당2", "주소2");
         Restaurant restaurant3 = createTestRestaurant(1L, 1L, "식당3", "주소3");
