e8c2e1ac589a131b462402c2542a95437a6d2a21
kevstevie <jianna442@gmail.com>
2024-07-14 21:13:39 +0900

    chore: spring 버전업에 따른 import 수정

--- matzip-core/src/main/java/com/woowacourse/matzip/domain/image/UnusedImage.java
+++ matzip-core/src/main/java/com/woowacourse/matzip/domain/image/UnusedImage.java
@@ -1,13 +1,14 @@
 package com.woowacourse.matzip.domain.image;
 
 import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
