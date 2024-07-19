0edeb5f6b3d4c168ca87c82afc398efab1c5b670
kevstevie <jianna442@gmail.com>
2024-07-19 16:22:44 +0900

    refactor: 이미지 업로드 트랜잭션 제거

--- matzip-app-external-api/src/main/java/com/woowacourse/matzip/application/image/ImageService.java
+++ matzip-app-external-api/src/main/java/com/woowacourse/matzip/application/image/ImageService.java
@@ -4,17 +4,10 @@ import com.woowacourse.matzip.application.response.ImageUploadResponse;
 import com.woowacourse.matzip.domain.image.ImageExtension;
 import com.woowacourse.matzip.domain.image.UnusedImage;
 import com.woowacourse.matzip.domain.image.UnusedImageRepository;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Service
-@Transactional(readOnly = true)
 public class ImageService {
 
     private final ImageManager imageManager;
@@ -25,7 +18,6 @@ public class ImageService {
         this.unusedImageRepository = unusedImageRepository;
     }
 
-    @Transactional
     public ImageUploadResponse uploadImage(final MultipartFile file) {
         String originalFileName = file.getOriginalFilename();
         ImageExtension.validateExtension(originalFileName);
